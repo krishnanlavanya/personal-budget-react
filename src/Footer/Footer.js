51e41936090e1d48facdbd493bb545a8066c55f3
Lavanya Krishnan <125098703+krishnanlavanya@users.noreply.github.com>
2023-09-30 19:08:13 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center">
+        All rights reserved &copy; Lavanya Krishnan
+    </div>
+    </footer>
+  );
+}
+
+export default Footer;
